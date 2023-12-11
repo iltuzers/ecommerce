eaaa76d3a8ec1f14cb14b4923427bd8034f0be51
iltuzers <iltuzers@gmail.com>
2023-12-10 22:20:51 -0500

    Refactor code

--- store/models.py
+++ store/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'categories'
-    
+
     def get_absolute_url(self):
         return reverse('store:category_list', args=[self.slug])
 
@@ -17,27 +17,33 @@ class Category(models.Model):
         return self.name
 
 
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
+
+
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255, default='admin') # author of the book
+    author = models.CharField(max_length=255, default='admin')  # author of the book
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/') # doesn't store image in db, only link
+    image = models.ImageField(upload_to='images/', default='images/default.jpg')  # doesn't store image in db
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     in_stock = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    products = ProductManager()
 
     class Meta:
-        verbose_name_plural = 'Products' # doesn't really need this
+        verbose_name_plural = 'Products'  # doesn't really need this
         ordering = ('-created',)
 
     def get_absolute_url(self):
         return reverse('store:product_detail', args=[self.slug])
-
 
     def __str__(self):
         return self.title
--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -1,31 +1,30 @@
-from unittest import skip
-
 from django.contrib.auth.models import User
 from django.http import HttpRequest
 from django.test import Client, RequestFactory, TestCase
 from django.urls import reverse
 
 from store.models import Category, Product
-from store.views import all_products
+from store.views import product_all
 
 
 class TestViewResponses(TestCase):
-
     def setUp(self):
         self.c = Client()
         self.factory = RequestFactory()
         User.objects.create(username='admin')
         Category.objects.create(name='django', slug='django')
-        Product.objects.create(category_id=1, title='django beginners',  created_by_id=1,
+        Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
                                slug='django-beginners', price='20.00', image='django')
-    
+
     def test_url_allowed_hosts(self):
         """
         Test allowed hosts
         """
-        response = self.c.get('/')
+        response = self.c.get('/', HTTP_HOST='noaddress.com')
+        self.assertEqual(response.status_code, 400)
+        response = self.c.get('/', HTTP_HOST='yourdomain.com')
         self.assertEqual(response.status_code, 200)
-    
+
     def test_product_detail_url(self):
         """
         Test Product response status
@@ -39,20 +38,19 @@ class TestViewResponses(TestCase):
         """
         response = self.c.get(reverse('store:category_list', args=['django']))
         self.assertEqual(response.status_code, 200)
-    
+
     def test_homepage_html(self):
         request = HttpRequest()
-        response = all_products(request)
+        response = product_all(request)
         html = response.content.decode('utf8')
-        self.assertIn('<title>Home</title>', html)
+        self.assertIn('<title>BookStore</title>', html)
         self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
         self.assertEqual(response.status_code, 200)
-    
+
     def test_view_function(self):
-        request = self.factory.get('/item/django-beginners')
-        response = all_products(request)
+        request = self.factory.get('/django-beginners')
+        response = product_all(request)
         html = response.content.decode('utf8')
-        self.assertIn('<title>Home</title>', html)
+        self.assertIn('<title>BookStore</title>', html)
         self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
         self.assertEqual(response.status_code, 200)
-        
